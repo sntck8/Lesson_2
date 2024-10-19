841312cb9d26518b65ed67db0b66324b5ca84eed
sntck8 <sntck88@gmail.com>
2024-10-19 10:36:03 +0700

    Add files via upload
    Домашняя работа по уроку "Условная конструкция. Операторы if, elif, else"

---
+++ module_2_2.py
@@ -0,0 +1,9 @@
+first = input("Введите первое число: ")
+second = input("Введите второе число: ")
+third = input("Введите третье число: ")
+if first == second and first == third:
+    print("Все числа равны!")
+elif first == second or  second == third or third == first:
+    print("Равны два из трех чисел!")
+elif first != second or  second != third or third != first:
+    print("Равных чисел нет!")
